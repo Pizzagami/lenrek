c9c84a121318779088da18353e2ec1a75c7e8ad9
pzgm <dev@pizzagami.fr>
2024-05-23 16:24:17 +0200

    multiboot header issues

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-
+#![feature(naked_functions)]
 
 mod vga;
 mod gdt;
@@ -9,8 +9,37 @@ fn print_gdt() {
     gdt::print_gdt();
 }
 
+#[allow(dead_code)]
+pub struct MultibootHeader {
+    magic: u32,
+    arch: u32,
+    magic2: u32
+}
+
 #[no_mangle]
-pub extern "C" fn main() {
+#[link_section = ".multiboot"]
+pub static MULTIBOOT: MultibootHeader = MultibootHeader {
+    magic: 0x1BADB002,
+    arch: 0x0,
+    magic2: -(0x1BADB002 as i32) as u32
+};
+
+#[naked]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    unsafe {
+        core::arch::asm!("
+            cli
+            mov esp, 0xf00000
+            call main
+            cli
+            hlt
+        ", options(noreturn));
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
 
     let mut cell = vga::Cell::default();
     cell.reset_screen();
