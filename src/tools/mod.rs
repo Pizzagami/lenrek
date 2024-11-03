4011156e239d749d5f472018f8b3fb8443822a92
imaga <pizz@gami.fr>
2024-11-03 14:28:52 +0100

    kfs 3

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,10 +1,7 @@
-//! Utility functions
-
-/// Serial port driver
 pub mod debug;
 
-/// Inbound and outbound port I/O functions
 pub mod io;
 
-/// Library functions
 pub mod librs;
+
+pub mod video_graphics_array;
